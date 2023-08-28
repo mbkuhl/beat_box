80a177c438836d223d7f89ec1b82678fd12ab523
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-27 21:53:48 -0600

    Write test for greenlist add sounds

--- spec/greenlist_spec.rb
+++ spec/greenlist_spec.rb
@@ -11,7 +11,23 @@ RSpec.describe Greenlist do
     it 'has a starter greenlist when created' do
       greenlist = Greenlist.new
 
-      expect(greenlist.sounds).to eq(["tee", "dee", "deep", "bop", "boop", "la", "na",])
+      expect(greenlist.sounds).to eq(["tee", "dee", "deep", "bop", "boop", "la", "na"])
+    end
+  end
+
+  describe '#add_sounds' do
+    it 'can add new sounds to greenlist' do
+      greenlist = Greenlist.new
+      greenlist.add_sounds("boom")
+
+      expect(greenlist.sounds).to eq(["tee", "dee", "deep", "bop", "boop", "la", "na", "boom"])
+    end
+
+    it 'can add multiple sounds to greenlist' do
+      greenlist = Greenlist.new
+      greenlist.add_sounds("boom bam")
+
+      expect(greenlist.sounds).to eq(["tee", "dee", "deep", "bop", "boop", "la", "na", "boom", "bam"])
     end
   end
 end
