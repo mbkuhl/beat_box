fd293719b0334297ad1c77aa7b82c658444a87f7
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-27 22:05:35 -0600

    Pass greenlist add sounds tests

--- lib/greenlist.rb
+++ lib/greenlist.rb
@@ -3,4 +3,9 @@ class Greenlist
   def initialize
     @sounds = ["tee", "dee", "deep", "bop", "boop", "la", "na",]
   end
+
+  def add_sounds(sounds)
+    sounds_array = sounds.split(" ")
+    @sounds += sounds_array
+  end
 end
