99c782795d42b5040b5e2a02869d5b71de677060
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-27 22:22:54 -0600

    Pass greenlist remove sounds tests

--- lib/greenlist.rb
+++ lib/greenlist.rb
@@ -8,4 +8,13 @@ class Greenlist
     sounds_array = sounds.split(" ")
     @sounds += sounds_array
   end
+
+  def remove_sounds (sounds)
+    sounds_array = sounds.split(" ")
+    sounds_array.each do |sound|
+      if @sounds.include?(sound)
+        @sounds.delete(sound)
+      end
+    end
+  end
 end
